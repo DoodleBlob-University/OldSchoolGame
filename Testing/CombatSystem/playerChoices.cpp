32354dd0cc80b71cf910263f9e34b855946091a3
George Franklin (frankl33) <frankl33@coventry.ac.uk>
2018-02-05 16:27:30 +0000

    More Options for the user
    This adds a selection for the user when it encounters an enemy, the options aren't known but will be when the implamentation has occured with the database

--- Testing/CombatSystem/playerChoices.cpp
+++ Testing/CombatSystem/playerChoices.cpp
@@ -8,32 +8,71 @@ int main(){
     //display options to the user for each situation, ncurses helps with the interface but this is the simple logic 
 
     string const attackSearch = "attack";
+    string const defenseSearch = "defense";
+    string const bagSearch = "bag";
     string response;
       
     
     cout << "What would you like to do?" << endl;
     getline(cin, response);
-    cout << response << endl;
-    
-    if (cin.fail()){
-    const int len = strlen(response.c_str());
-    }
+    //cout << response << endl;
 
     for (size_t i = 0; response.size() > i; ++i){
-      if ((response[i] >= 'A') && (response[i] <= 'Z')) response[i] = response[i] - 'A' + 'a';
+      if ((response[i] >= 'A') && (response[i] <= 'Z')){
+          response[i] = response[i] - 'A' + 'a';
+      }
     }
-    cout << response << endl;
-
-    //response.erase(std::remove(response.begin(), response.end(), ' '), response.end());
-    //cout << response << endl; - not needed anymore thanks to the getline(), removes spaces
-    size_t pos;
-    pos = response.find(attackSearch);
+    //cout << response << endl;
     
-    if (pos != string::npos){ //string::npos = reference to the strings input http://www.cplusplus.com/reference/string/string/npos/ 
-        cout << "It works" << endl;
+    size_t findAttack = response.find(attackSearch);
+    if (findAttack!=string::npos){
+        //cout << "attack works" << endl;
+        string attackResponse;
+        string const dagger = "dagger"; //these options could use a DB call for what kind of options, can only have 3 options to limit code and options, same for all 'string const' - s
+        string const bow = "bow";
+        string const sword = "sword";
+        cout << "What attack would you like to do?" << endl;
+        getline(cin, attackResponse);
+        for (size_t i = 0; response.size() > i; ++i){
+              if ((attackResponse[i] >= 'A') && (attackResponse[i] <= 'Z')){
+                  attackResponse[i] = attackResponse[i] - 'A' + 'a'; //duplication of code
+              }
+        }
+        size_t attackDagger = attackResponse.find(dagger);
+        if (attackDagger!=string::npos){
+            cout << "dagger works" << endl;
+        }
+        size_t attackBow = attackResponse.find(bow);
+        if (attackBow!=string::npos){
+            cout << "bow works" << endl;
+        }
+        size_t attackSword = attackResponse.find(sword);
+        if (attackSword!=string::npos){
+            cout << "dagger works" << endl;
+        }
     }
-    else {
-        cout << "Not included" << endl;
-    }
+    
+    size_t findDefense = response.find(defenseSearch);
+    if (findDefense!=string::npos){
+        //cout << "defense works" << endl;
+        string defenseResponse;
+        string const heal = "heal";
+        string const block = "block";
+        cout << "What defense would you like to do?" << endl;
+        getline(cin, defenseResponse);
+        for (size_t i = 0; response.size() > i; ++i){
+              if ((response[i] >= 'A') && (response[i] <= 'Z')){
+                  response[i] = response[i] - 'A' + 'a';//duplication of code 
+              }
+        }
+        size_t defenseHeal = defenseResponse.find(heal);
+        if (defenseHeal!=string::npos){
+            cout << "heal works" << endl;
+        }
+        size_t defenseBlock = defenseResponse.find(block);
+        if (defenseBlock!=string::npos){
+            cout << "block works" << endl;
+        }
+    }    
     return 0;
 }
