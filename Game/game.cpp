50c9416a7bcfca0e66b3d7efcf0f1f47e754b7c9
smithw7@coventry.ac.uk <smithw7@coventry.ac.uk>
2018-03-15 15:29:53 +0000

    Added "About" to function options menu

--- Game/game.cpp
+++ Game/game.cpp
@@ -377,7 +377,7 @@ class Game{//Charles Barry
     int MainMenu(Map main){//Charles Barry
       string dungeonname = main.getName();
       int selected = 0; int suboption = 0;
-      vector<vector<string>> MenuOptions = {{"Play Game","Extra", "Options", "Exit"},{"MapCreator","Back"},{"----","Back"}};
+      vector<vector<string>> MenuOptions = {{"Play Game","Extra", "Options", "Exit"},{"MapCreator","Back"},{"About","Back"}};
       while(true){
         printDungeonName(dungeonname);
         mvwprintw(stat->getData(), 20, centreTextCursorPos("h: Help"), "h: Help");
@@ -407,7 +407,10 @@ class Game{//Charles Barry
                 {switch(suboption){
                   case 0: return 0; break;
                   case 1: {MapCreator creator(game->getData(), stat->getData(), term->getData(), maptiles, func);} return 2; break;
-                  case 2: break;
+                  case 2: 
+                  func->eraseTerminal();
+                  func->printTerminalText("Game created by:\nCharlie Barry, Will Smith, Matt Fretwell\nGeorge Franklin, Kai Arnold, Shirin Shahali\n\nOur inspiration for the game was taken from\nZork, Hobbit, D&D board game.\n\nEnjoy Not a Dungeon Crawler!!");
+                  break;
                 }}
                 break;
               case 1:
