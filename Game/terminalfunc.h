751b8297303cb259b4eb98d93506a0d1189ae9b9
barryc2 <barryc2@coventry.ac.uk>
2018-03-15 15:37:01 +0000

    Fixed bug in terminalfunc

--- Game/terminalfunc.h
+++ Game/terminalfunc.h
@@ -12,6 +12,7 @@ public:
   void printTerminalText(std::string text);
   WINDOW* getStat(){return stat;}
   std::string getUserInput();
+  std::string getUserInput(bool secure);
   bool getUserYN();
   void eraseTerminal();
   TerminalFunctions(WINDOW* _stat, WINDOW* _term, int _windowWidth);
