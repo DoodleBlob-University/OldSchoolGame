92e529545649a5405d4b4e386136cedd936497f5
Kai Arnold <arnoldk5@coventry.ac.uk>
2018-03-16 03:17:28 +0000

    Finished Inventory System

--- Inventory/testInventory.cpp
+++ Inventory/testInventory.cpp
@@ -9,18 +9,18 @@ using namespace std;
 
 class Item {
     private:
-        int itemID;
         int cost;
         string name;
         int itemType;
+    protected:
+      void setItemValues(string _name, int _cost, int _itemType){
+        name = _name; cost = _cost; itemType = _itemType;
+      }
     public:
-        int getID(){return itemID;}
         string getName(){return name;}
         int getCost(){return cost;}
         int getType(){return itemType;}
-        Item(int _itemID, string _name, int _cost, int _itemType){
-            itemID = _itemID; name = _name; cost = _cost; itemType = _itemType;
-        }
+        Item(){}
 };
 
 class Weapon : public Item {
@@ -28,24 +28,28 @@ class Weapon : public Item {
         int damage;
 
     public:
-        Weapon(int ID, string name, int _damage, int cost) : Item(ID, name, cost, 0){
-            damage = _damage;
+        void setValues(string name, int _damage, int cost){
+          damage = _damage;
+          setItemValues(name, cost, 0);
         }
         int getDamage(){
             return damage;
         }
+        Weapon() : Item(){}
     };
 
 class Armour : public Item {
     private:
         int armour;
     public:
-        Armour(int ID, string name, int _armour, int cost) : Item(ID, name, cost, 1){
-            armour = _armour;
+        void setValues(string name, int _armour, int cost){
+          armour = _armour;
+          setItemValues(name, cost, 1);
         }
         int getArmour(){
             return armour;
         }
+        Armour() : Item(){}
 };
 
 class Potion : public Item {
@@ -60,17 +64,20 @@ class Potion : public Item {
         int getMana(){
             return mana;
         }
-        Potion(int ID, string name, int _health, int _mana, int cost) : Item(ID, name, cost, 2){
-            health = _health; mana = _mana;
+        void setValues(string name, int _health, int _mana, int cost){
+          health = _health; mana = _mana;
+          setItemValues(name, cost, 2);
         }
+
+        Potion() : Item(){}
 };
 
 class PlayerInventory{
     private:
-        struct Inventory
-            Weapon *Weapon;
-            Armour *Armour;
-            Potion *Potion;
+        struct Inventory{
+            Weapon* w = new Weapon();
+            Armour* a = new Armour();
+            Potion* p = new Potion();
         };
         void getWeapon() {
             int count = 1;
@@ -79,21 +86,17 @@ class PlayerInventory{
             cur->set_sql("SELECT item.id, weapon.name, weapon.damage, weapon.cost FROM weapon, item WHERE item.WeaponID = weapon.ID;");
             cur->prepare();
             while(cur->step()){
-                int id = cur->get_int(0);
-                Weapon* wea = new Weapon(id, cur->get_text(1), cur->get_int(2), cur->get_int(3));
-                items[id].Weapon = wea;
+                items[cur->get_int(0)].w->setValues(cur->get_text(1), cur->get_int(2), cur->get_int(3));
             }
         }
         void getArmour() {
             int count = 1;
             sqlite::sqlite db("Database.db");
             auto cur = db.get_statement();
-            cur->set_sql("SELECT item.id, armour.name, armour.Armour, armour.cost FROM armour, item WHERE item.ArmourID = armour.iD;");
+            cur->set_sql("SELECT item.id, armour.name, armour.Armour, armour.cost FROM armour, item WHERE item.ArmourID = armour.ID;");
             cur->prepare();
             while(cur->step()){
-                int id = cur->get_int(0);
-                Armour* arm = new Armour(id, cur->get_text(1), cur->get_int(2), cur->get_int(3));
-                items[id].Armour = arm;
+                items[cur->get_int(0)].a->setValues(cur->get_text(1), cur->get_int(2), cur->get_int(3));
             }
         }
         void getPotion() {
@@ -102,9 +105,7 @@ class PlayerInventory{
             cur->set_sql("SELECT item.id, potion.name, potion.health, potion.mana, potion.cost FROM potion, item where item.PotionID = potion.ID;");
             cur->prepare();
             while(cur->step()){
-                int id = cur->get_int(0);
-                Potion* pot = new Potion(id, cur->get_text(1), cur->get_int(2), cur->get_int(3), cur->get_int(4), cur->get_int(5));
-                items[id].Potion = pot;
+                items[cur->get_int(0)].p->setValues(cur->get_text(1), cur->get_int(2), cur->get_int(3), cur->get_int(4));
             }
         }
         int getItemCount() {
@@ -118,46 +119,81 @@ class PlayerInventory{
 
             return size;
         }
+        Weapon* wempty = new Weapon();
+        Armour* aempty = new Armour();
+        Potion* pempty = new Potion();
+        int itemno = 0;
     public:
         vector<Inventory> items;
-        vector<Inventory> playerInventory;
-        vector<Inventory> playerEquipped;
+        vector<Inventory> playerInv;
+        vector<Inventory> playerEquip;
 
         PlayerInventory(){
-            items.resize(getItemCount());
+            itemno = getItemCount() + 1;
+            items.resize(itemno);
+            playerInv.resize(10);
+            playerEquip.resize(3);
             getWeapon();
             getArmour();
             getPotion();
         }
         void addItem(int id){
-          int emptypos;
-          for(int i = 1; i < playerInventory.size(); ++i){
-            if(playerInventory[i].Weapon = NULL && playerInventory[i].Armour = NULL && playerInventory[i].Potion = NULL){
+          int emptypos = -1;
+          for(int i = 0; i < playerInv.size(); ++i){
+            if(playerInv[i].w->getName() == "" && playerInv[i].a->getName() == "" && playerInv[i].p->getName() == ""){
               emptypos = i;
               break;
             }
           }
-          if(!emptypos){
-            //no adding here
+          //playerInventory[1] = items[id];
+          if(emptypos >= 0){
+            if(id <= itemno){
+            playerInv[emptypos] = items[id];
+          }//ur a fgt
           }else{
-            playerInventory[emptypos] = items[id];
+            //is empty bois
           }
         }
-        void removeItem(int id){
-
+        void removeItem(int pos){
+          playerInv[pos].w = wempty;
+          playerInv[pos].a = aempty;
+          playerInv[pos].p = pempty;
         }
+        void equipItem(int invpos, int equipslot){
+          Inventory temp = playerEquip[equipslot];
+          playerEquip[equipslot] = playerInv[invpos];
+          playerInv[invpos] = temp;
+        }
+
 };
 
 
 int main() {
     PlayerInventory* inv = new PlayerInventory();
-    //inv->addItem(2);
-    inv->addItem(2);
+    inv->addItem(1);
+    inv->addItem(1);
+    inv->addItem(1);
+    inv->addItem(1);
+    inv->addItem(1);
+    inv->addItem(1);
+    inv->addItem(1);
+    inv->addItem(1);
+    inv->addItem(1);
+    inv->addItem(1);
+    inv->removeItem(2);
+    inv->addItem(10);
+
+    inv->equipItem(3, 1);
 
     initscr();
-    printw("%s", inv->playerInventory[1].Weapon->getName().c_str());
+    for(int i = 0; i < 10; ++i){
+      printw("-%s\n", inv->playerInv[i].w->getName().c_str());
+    }
+    printw("\n\n");
+    for(int i = 0; i < 3; ++i){
+      printw("-%s\n", inv->playerEquip[i].w->getName().c_str());
+    }
     getch();
     endwin();
-
 
 }
